64e8dfd2844288a5149d39b81bce8ed4e3bdc5c6
Gabriel Oliveira Gomes <00001090692699sp@aluno.educacao.sp.gov.br>
2023-11-23 12:35:01 -0300

    crud feito

--- screens/Services.js
+++ screens/Services.js
@@ -1,12 +1,12 @@
-import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TextInput, View, RefreshControl, Modal, Pressable, Touchable, TouchableOpacity } from 'react-native'
 import { epilogueFont, interFont } from '../assets/fonts/fontsExport';
 import Pen from '../assets/svg/services/pen';
 import Trash from '../assets/svg/services/trash';
-import ServicesRow from '../components/services/ServicesRow';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useAuth } from '../utils/handleAuth';
 import axios from 'axios';
 import Lupa from '../assets/svg/crudFunc/lupa';
+import Plus from '../assets/svg/services/plus';
 
 function Services({ navigation }) {
 
@@ -24,6 +24,38 @@ function Services({ navigation }) {
   const [isLogged, setIsLogged] = useState(false)
   const [isEmpregado, setIsEmpregado] = useState('')
 
+  const [changed, setChanged] = useState(0)
+
+  const [refreshing, setRefreshing] = useState(false)
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [addModalVisible, setAddModalVisible] = useState(false);
+  
+  const [idToChange, setIdToChange] = useState('')
+  
+  const [skipFirstIteration, setSkipFirstIteration] = useState(false)
+  
+
+  const [changeName, setChangeName] = useState('')
+  const [changeGarantia, setChangeGarantia] = useState('')
+  const [changeContrato, setChangeContrato] = useState('')
+  const [changeRecursos, setChangeRecursos] = useState('')
+  const [changeObjetivo, setChangeObjetivo] = useState('')
+  const [changeTermos, setChangeTermos] = useState('')
+
+
+  const [addNome, setAddNome] = useState('')
+  const [addGarantia, setAddGarantia] = useState('')
+  const [addContrato, setAddContrato] = useState('')
+  const [addRecursos, setAddRecursos] = useState('')
+  const [addObjetivo, setAddObjetivo] = useState('')
+  const [addTermos, setAddTermos] = useState('')
+  const [addEmpregadoId, setEmpregadoId] = useState('')
+
+  const [translateQuantity, setTranslateQuantity] = useState(0)
+
+
     useEffect(() => {
       async function getAuth(){
         const [returnAuthData, authIsLogged, authIsEmpregado] = await useAuth();
@@ -35,7 +67,27 @@ function Services({ navigation }) {
       getData()
     }, [])
 
+    useEffect(() => {
+      setTranslateQuantity(0);
+    }, [addModalVisible, modalVisible])
+
+    useEffect(() => {
+      if(skipFirstIteration == false){
+        setSkipFirstIteration(true)
+      }
+      else{
+        setModalVisible(true)
+      }
+    }, [changeName])
+
+
+    useEffect(() => {
+      console.log(idToChange)
+    }, [idToChange])
+
+
     const getData = async () => {
+      setRefreshing(true)
       try {
         const res = await axios.get("http://10.0.2.2:3000/servico")
         setData(res.data)
@@ -44,13 +96,114 @@ function Services({ navigation }) {
       catch(err){
         console.log(err.message)
       }
+      setRefreshing(false)
+
     }
+    
+
+    const handleDelete = async (deleteId) => {
+      try{
+        const res = await axios.delete(`http://10.0.2.2:3000/servico/${deleteId}`)
+        getData()
+      }
+      catch(error){
+        console.error(error)
+      }
+    }
+    
+
+    onFocusAnimation = async (input) => {
+      if(input == 'nome'){
+        setTranslateQuantity(300)
+      }
+
+      if(input == 'garantia'){
+        setTranslateQuantity(150)
+      }
+      if(input == 'contrato'){
+        setTranslateQuantity(0)
+      }
+
+      if(input == 'recursos'){
+        setTranslateQuantity(-150)
+      }
+
+      if(input == 'objetivo'){
+        setTranslateQuantity(-150)
+      }
+
+      if(input == 'termos'){
+        setTranslateQuantity(-150)
+      }
+    }
+
+    const handleChange = async () => {
+      if(changeName != ''){
+        try{
+          const res = await axios.patch(`http://10.0.2.2:3000/servico/${idToChange}`, {
+            nome: changeName,
+            garantia: changeGarantia,
+            detalhesContrato: changeContrato,
+            recursos: changeRecursos,
+            termosCondicoes: changeTermos,
+            objetivo: changeObjetivo
+          })
+          setModalVisible(false)
+          getData()
+        }
+        catch(error){
+          console.error(error)
+        }
+      }
+    }
+
+    const handleAdd = async () => {
+      if(addNome != '' && addGarantia != '' && addContrato != '' && addRecursos != '' && addTermos != '' && addObjetivo != ''){
+        try{
+          const res = await axios.post(`http://10.0.2.2:3000/servico`, {
+            nome: addNome,
+            garantia: addGarantia,
+            detalhesContrato: addContrato,
+            recursos: addRecursos,
+            termosCondicoes: addTermos,
+            objetivo: addObjetivo,
+            empregadoId: authData.id
+          })
+          setAddModalVisible(false)
+          getData()
+          clearAddInputs()
+        }
+        catch(error){
+          console.error(error)
+        }
+      }
+    }
+
+    const clearAddInputs = () => {
+      setAddNome('')
+      setAddContrato('')
+      setAddGarantia('')
+      setAddObjetivo('')
+      setAddRecursos('')
+      setAddTermos('')
+    }
+
+    const handleChangeClick = (nome, garantia, contrato, recursos, objetivo, termos, empregadoId, id) => {
+      setChangeName(nome)
+      setChangeGarantia(garantia)
+      setChangeContrato(contrato)
+      setChangeRecursos(recursos)
+      setChangeObjetivo(objetivo)
+      setChangeTermos(termos)
+      setIdToChange(id)
+    }
+
   if (!interLoaded || !epilogueLoaded) {
     return null;
   }
 
   return (
-    <View className="mb-24 w-screen flex flex-col items-center pt-10">
+    <View className="mb-40 w-screen flex flex-col items-center pt-10">
        <View className="flex flex-row items-center">
         <TextInput style={styles.shadowInput}
         value={search}
@@ -58,10 +211,6 @@ function Services({ navigation }) {
         placeholder="Pesquise aqui!" />
         <Lupa />
       </View>
-      <ScrollView>
-    
-
-        <View className="w-screen h-full flex flex-col items-center">
         <Text
         style={{
           fontFamily: "EpilogueRegular",
@@ -71,13 +220,188 @@ function Services({ navigation }) {
       >
         SERVIÇOS
       </Text>
+      <TouchableOpacity onPress={() => setAddModalVisible(true)} className="absolute right-5 top-32">
+      <Plus  />
+      </TouchableOpacity>
       <Text style={{fontFamily: 'InterSemiBold', fontSize: 16}}>
       Seus serviços
       </Text>
+      <ScrollView
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={getData} />}
+      >
+        <View className="w-screen h-full flex flex-col items-center">
+      
       {loaded && data.filter(servico => ((servico.nome.toUpperCase().includes(search.toUpperCase())) && (servico.empregadoId == authData.id))).map(servico => {
-        return(<ServicesRow key={servico.id} name={servico.nome} />)
+        return(
+          <>
+          <View className="w-full flex flex-row gap-7 mt-7 items-center justify-center mb-5">
+          <Pen onPress={() => handleChangeClick(servico.nome, servico.garantia, servico.detalhesContrato, servico.recursos, servico.objetivo, servico.termosCondicoes, servico.id)} />
+          <Text className="h-7 w-60 text-center overflow-hidden text-ellipsis" style={{ fontFamily: 'InterSemiBold', fontSize: 16 }}>
+              {servico.nome}
+          </Text>
+          <Trash onPress={() => {handleDelete(servico.id)}} />
+          </View>
+      <View className="w-full h-5" style={{ backgroundColor: "#53389E" }}>
+          </View>
+          </>
+          )
       })}
     </View>
+    <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+          <View style={styles.centeredView}>
+          <View style={[styles.modalView, { transform: [{translateY: translateQuantity}]}]}>
+
+            <Text className="text-xl">Nome:</Text>
+            <TextInput style={styles.shadowInput}
+        value={changeName}
+        onChangeText={(text) => setChangeName(text)}
+        onFocus={() => onFocusAnimation('nome')}
+        placeholder="Digite o Nome" />
+
+
+        <Text className="text-xl">Garantia:</Text>
+            <TextInput style={styles.shadowInput}
+        value={changeGarantia}
+        onChangeText={(text) => setChangeGarantia(text)}
+        onFocus={() => onFocusAnimation('garantia')}
+        placeholder="Digite a Garantia" />
+
+
+        <Text className="text-xl">Detalhes Contrato:</Text>
+            <TextInput style={styles.shadowInput}
+        value={changeContrato}
+        onChangeText={(text) => setChangeContrato(text)}
+        onFocus={() => onFocusAnimation('contrato')}
+        placeholder="Digite os Detalhes do Contrato" />
+
+
+        <Text className="text-xl">Recursos:</Text>
+            <TextInput style={styles.shadowInput}
+        value={changeRecursos}
+        onChangeText={(text) => setChangeRecursos(text)}
+        onFocus={() => onFocusAnimation('recursos')}
+        placeholder="Digite os Recursos" />
+
+
+        <Text className="text-xl">Objetivo:</Text>
+            <TextInput style={styles.shadowInput}
+        value={changeObjetivo}
+        onChangeText={(text) => setChangeObjetivo(text)}
+        onFocus={() => onFocusAnimation('objetivo')}
+        placeholder="Digite o Objetivo" />
+
+
+        <Text className="text-xl">Termos e Condições:</Text>
+            <TextInput style={styles.shadowInput}
+        value={changeTermos}
+        onChangeText={(text) => setChangeTermos(text)}
+        onFocus={() => onFocusAnimation('termos')}
+        placeholder="Digite os Termos e Condições" />
+
+
+        <View className="flex flex-row items-center gap-5">
+          
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Cancelar</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonConfirm]}
+              onPress={() => handleChange()}>
+              <Text style={styles.textStyle}>Confirmar</Text>
+            </Pressable>
+        </View>
+          </View>
+        </View>
+      </Modal>
+
+
+
+
+
+
+
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={addModalVisible}
+        onRequestClose={() => {
+          setAddModalVisible(!addModalVisible);
+        }}>
+          <View style={styles.centeredView}>
+          <View style={[styles.modalView, { transform: [{translateY: translateQuantity}]}]}>
+            <Text className="text-xl">Nome:</Text>
+            <TextInput style={styles.shadowInput}
+        value={addNome}
+        onChangeText={(text) => setAddNome(text)}
+        onFocus={() => onFocusAnimation('nome')}
+        placeholder="Digite o Nome" />
+
+
+        <Text className="text-xl">Garantia:</Text>
+            <TextInput style={styles.shadowInput}
+        value={addGarantia}
+        onChangeText={(text) => setAddGarantia(text)}
+        onFocus={() => onFocusAnimation('garantia')}
+        placeholder="Digite a Garantia" />
+
+
+        <Text className="text-xl">Detalhes Contrato:</Text>
+            <TextInput style={styles.shadowInput}
+        value={addContrato}
+        onChangeText={(text) => setAddContrato(text)}
+        onFocus={() => onFocusAnimation('contrato')}
+        placeholder="Digite os Detalhes do Contrato" />
+
+
+        <Text className="text-xl">Recursos:</Text>
+            <TextInput style={styles.shadowInput}
+        value={addRecursos}
+        onChangeText={(text) => setAddRecursos(text)}
+        onFocus={() => onFocusAnimation('recursos')}
+        placeholder="Digite os Recursos" />
+
+
+        <Text className="text-xl">Objetivo:</Text>
+            <TextInput style={styles.shadowInput}
+        value={addObjetivo}
+        onChangeText={(text) => setAddObjetivo(text)}
+        onFocus={() => onFocusAnimation('objetivo')}
+        placeholder="Digite o Objetivo" />
+
+
+        <Text className="text-xl">Termos e Condições:</Text>
+            <TextInput style={styles.shadowInput}
+        value={addTermos}
+        onChangeText={(text) => setAddTermos(text)}
+        onFocus={() => onFocusAnimation('termos')}
+        placeholder="Digite os Termos e Condições" />
+
+
+        <View className="flex flex-row items-center gap-5">
+          
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setAddModalVisible(!addModalVisible)}>
+              <Text style={styles.textStyle}>Cancelar</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonConfirm]}
+              onPress={() => handleAdd()}>
+              <Text style={styles.textStyle}>Confirmar</Text>
+            </Pressable>
+        </View>
+          </View>
+        </View>
+      </Modal>
     </ScrollView>
     </View>
   )
@@ -94,6 +418,51 @@ const styles = StyleSheet.create({
     width: 300,
     margin: 12,
     padding: 10,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: 350,
+    height: 800
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#DD0004',
+  },
+  buttonConfirm: {
+    backgroundColor: '#53389E',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    textAlign: 'center',
   },
 });
 
--- screens/ShowCaseScreen.js
+++ screens/ShowCaseScreen.js
@@ -4,26 +4,28 @@ import { Button, View } from 'react-native'
 function ShowCaseScreen({ navigation }) {
   return (
     <View className="h-screen w-screen flex flex-col">
-        <View className="h-10"></View>
+        <View className="h-5"></View>
         <Button title='Home' onPress={() => navigation.navigate('Home')} />
-        <View className="h-10"></View>
+        <View className="h-5"></View>
         <Button title='Login' onPress={() => navigation.navigate('Login')} />
-        <View className="h-10"></View>
+        <View className="h-5"></View>
 
         <Button title='Esqueci a Senha' onPress={() => navigation.navigate('Esqueci a Senha')} />
-        <View className="h-10"></View>
+        <View className="h-5"></View>
 
         <Button title='Cadastro' onPress={() => navigation.navigate('Cadastro')} />
-        <View className="h-10"></View>
+        <View className="h-5"></View>
         <Button title='Sobre Nós' onPress={() => navigation.navigate('Sobre Nós')} />
-        <View className="h-10"></View>
+        <View className="h-5"></View>
         <Button title='Serviços' onPress={() => navigation.navigate('Serviços')} />
-        <View className="h-10"></View>
+        <View className="h-5"></View>
         <Button title='Crud Serviços' onPress={() => navigation.navigate('Crud Serviços')} />
-        <View className="h-10"></View>
+        <View className="h-5"></View>
         <Button title='Crud Funcionários' onPress={() => navigation.navigate('Crud Funcionários')} />
-        <View className="h-10"></View>
+        <View className="h-5"></View>
         <Button title='Localização' onPress={() => navigation.navigate('Localização')} />
+        <View className="h-5"></View>
+        <Button title='Adicionar e Editar Serviço' onPress={() => navigation.navigate('Adicionar e Editar Serviço')} />
     </View>
   )
 }
